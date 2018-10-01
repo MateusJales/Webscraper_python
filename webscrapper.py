0f165498b376ef21fa5be93291fbe853c42b06fb
Mateus Jales <mateusjales@iMac-3.local>
2018-10-01 10:49:02 -0300

    Add save_list method to the SaraivaCrawler class and errors correction
    save_list method added and minnor errors corrected on the SaraivaCrawler class. Still need reparation on the format of the CSV file created and to add reusability to the program

--- webscrapper.py
+++ webscrapper.py
@@ -1,20 +1,20 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-
+from pandas import DataFrame
 # Can be used for any category of products in the site
 BASE_URL = "https://busca.saraiva.com.br/pages/games/playstation-4?page="
 ERROR_MENSAGE = "Não localizamos nenhum resultado para:"
 
 def real_to_float(real_str):
-    return float(real_str[2:].replace(".", "").replace(",", "."))
+    return float(real_str.replace("R$", "").replace(".", "").replace(",", "."))
 
 class SaraivaCrawler(object):
 
     def __init__(self):
         options = webdriver.ChromeOptions()
-        options.add_argument('headless')
-        options.add_argument('window-size=1920x1080')
-        self.driver = webdriver.Chrome(chrome_options=options)
+        # options.add_argument('headless')
+        # options.add_argument('window-size=1920x1080')
+        self.driver = webdriver.Chrome(options=options)
         self.items = []
 
     def craw_url(self, url):
@@ -26,9 +26,10 @@ class SaraivaCrawler(object):
                 break
             self.items += self.get_products(html)
             page += 1
+        self.driver.quit()
 
     def get_products(self, html):
-        ul = html.find_all("ul", {"class": "neemu-products-container nm-view-type-grid"})
+        ul = html.find("ul", {"class": "neemu-products-container nm-view-type-grid"})
         return ul.find_all("li")
 
     def get_products_names(self):
@@ -39,17 +40,42 @@ class SaraivaCrawler(object):
 
     def get_products_prices(self):
         prices = {
-            "base_price": [],
-            "pay_up_front_price": [],
+            "base": [],
+            "pay_up_front": [],
+            "on_sale": [],
         }
         for product in self.items:
             if product.find("div", {"class": "nm-price-container"}) is None:
-                prices["base_price"].append("Produto Indisponível")
-                prices["pay_up_front_price"].append(None)
+                prices["base"].append("Produto Indisponível")
+                prices["pay_up_front"].append(None)
+                prices["on_sale"].append(False)
             else:
-                prices["base_price"].append(real_to_float(product.find("div", {"class": "nm-price-container"}).text.strip("\n")))
+                prices["base"].append(real_to_float(product.find("div", {"class": "nm-price-value"}).text.rstrip()))
                 try:
-                    prices["pay_up_front_price"].append(real_to_float(product.find_all("strong")[1].text))
+                    prices["pay_up_front"].append(real_to_float(product.find_all("strong")[1].text.rstrip()))
                 except:
-                    prices["pay_up_front_price"].append(None)        
+                    prices["pay_up_front"].append(None)
+                if product.find("div",{"class":"nm-flag discount"}):
+                    prices["on_sale"].append(True)
+                else:
+                    prices["on_sale"].append(False)
         return prices
+    
+    def save_list(self):
+        names = self.get_products_names()
+        prices = self.get_products_prices()
+        df = DataFrame.from_dict(
+            {
+                'Product Name': names,
+                'Base Price': prices["base"],
+                'Pay Up Front Price': prices["pay_up_front"],
+                'On Sale?': prices["on_sale"]
+            }
+        )
+        cols = ['Product Name', 'Base Price', 'Pay Up Front Price', 'On Sale?']
+        df = df[cols]
+        df.to_csv('Products_Table.csv', index=False)
+
+crawler = SaraivaCrawler()
+crawler.craw_url(BASE_URL)
+crawler.save_list()
